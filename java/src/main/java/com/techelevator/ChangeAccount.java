d9dc426dab257766ae584033798d88300dfe99ba
Kevin Taylor <kevintaylor88@gmail.com>
2021-10-07 12:40:01 -0400

    Added change maker

---
+++ java/src/main/java/com/techelevator/ChangeAccount.java
@@ -0,0 +1,28 @@
+package com.techelevator;
+
+public class ChangeAccount {
+
+    private int balance;  //multiply input by 100 - dont need to convert back to dollars since change doesn't include dollars
+    private int quarter;
+    private int dime;
+    private int nickle;
+
+    public String makeChange() {
+        int quarters = 0;
+        int dimes = 0;
+        int nickles = 0;
+
+        while(balance >=25){
+            quarters++;
+            balance -= 25;
+        } while (balance >=10){
+            dimes++;
+            balance -=10;
+        } while (balance >= 5){
+            nickles++;
+            balance -=5;
+        }
+        return "Your change is " + quarters + " quarters, " + dimes + " dimes, " + nickles + " nickles.";
+    }
+
+}
--- java/src/main/java/com/techelevator/Product.java
+++ java/src/main/java/com/techelevator/Product.java
@@ -4,12 +4,33 @@ public abstract class Product {
 
     private String name;
     private int price;
+    private String slotLocation;
+    private String type;
+    //count (can be put into inventory class)
+    //abstract method for getsound to make sure children ints
+
+    public abstract String getSound();
 
     public Product(String name, int price) {
         this.name = name;
         this.price = price;
     }
 
+    public String getSlotLocation() {
+        return slotLocation;
+    }
+
+    public void setSlotLocation(String slotLocation) {
+        this.slotLocation = slotLocation;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 
     public String getName() {
         return name;
